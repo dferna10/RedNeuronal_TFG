d803b62aea79a759ca1045d855119bb205809e36
dferna10 <dferna10@estudiantes.unileon.es>
2021-03-22 09:51:06 +0100

    Codigo para renombrar las imagenes

---
+++ renombrado.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import numpy as np
+
+'''
+Funcion para obtener la ruta de las imagenes
+'''
+def getImagesPath(ruta):
+    dirname = os.path.join(os.getcwd(), ruta)
+    
+    return dirname + os.sep
+
+
+'''
+Función para leer las imagenes que vamos a procesar
+'''
+def readImagesToProcess(images_path, tam):
+    print("Lectura de imagenes de la ruta : " + images_path)
+    #Modificar para obtener los datos
+    images = []
+    directories = []
+    dircount = []
+    prevRoot = ''
+    cant = 0
+
+    print("Vamos a leer las imagenes de ",images_path)
+
+    for root, dirnames, filenames in os.walk(images_path):
+        if(images_path != root):
+            for filename in filenames:
+                if re.search("\.(jpg|jpeg|png|bmp|tiff|JPG)$", filename):
+                    cant = cant + 1
+                    filepath = os.path.join(root, filename)
+                    nuevo_nombre = os.path.join(root, "DJI_" + cant +".JPG")
+                    os.rename(filepath, nuevo_nombre)
+                    b = "Leyendo..." + str(cant)
+                    print (b, end="\r")
+                    if prevRoot != root:
+                        print(root, cant)
+                        prevRoot = root
+                        directories.append(root)
+                        dircount.append(cant)
+                        cant = 0
